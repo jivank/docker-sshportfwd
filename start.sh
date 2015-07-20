f46dbb1fe835aa0f1dd5fd5989576e0f7a27c406
Jivan Kulkarni <jivan@fa-eng.com>
2015-07-20 10:25:56 -0700

    changed ssh arguments

--- start.sh
+++ start.sh
@@ -4,5 +4,5 @@ if [ ! -f /root/.ssh/id_rsa.pub ]; then
     cat /dev/zero | ssh-keygen -q -N ""
 	cat /root/.ssh/id_rsa.pub
 fi
-ssh-keyscan  $DESTINATIONIP >> /root/.ssh/known_hosts
-ssh -R $DESTINATIONPORT:$LOCALIP:$LOCALPORT $DESTINATIONUSER@$DESTINATIONIP
+ssh-keyscan $DESTINATIONIP >> /root/.ssh/known_hosts
+ssh -nNT -L $DESTINATIONPORT:$LOCALIP:$LOCALPORT $DESTINATIONUSER@$DESTINATIONIP
