ef8ad0ffd10ec971a351423b760bb31b7d35b04f
Jivan Kulkarni <jivan@fa-eng.com>
2015-07-20 11:07:02 -0700

    fix start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
-if [ ! -f /root/.ssh/id_rsa.pub ]; then
-    cat /dev/zero | ssh-keygen -q -N ""
-	cat /root/.ssh/id_rsa.pub
+cat /root/.ssh/id_rsa.pub
+if [ ! -f /root/.ssh/id_rsa ]; then
+    cat /dev/zero | ssh-keygen -q -N ""    
+    cat /root/.ssh/id_rsa.pub
 fi
 ssh-keyscan $DESTINATIONIP >> /root/.ssh/known_hosts
-ssh -nNT -L $DESTINATIONPORT:$LOCALIP:$LOCALPORT $DESTINATIONUSER@$DESTINATIONIP
+ssh -nNT -R $DESTINATIONPORT:$LOCALIP:$LOCALPORT $DESTINATIONUSER@$DESTINATIONIP
