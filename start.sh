e10a377b25871183d3742b7bc81c9511aa7e1a1a
Jivan Kulkarni <jivan@fa-eng.com>
2015-07-20 09:59:08 -0700

    changed ~ to root

--- start.sh
+++ start.sh
@@ -4,5 +4,5 @@ if [ ! -f /root/.ssh/id_rsa.pub ]; then
     cat /dev/zero | ssh-keygen -q -N ""
 	cat /root/.ssh/id_rsa.pub
 fi
-ssh-keyscan  $DESTINATIONIP >> ~/.ssh/known_hosts
+ssh-keyscan  $DESTINATIONIP >> /root/.ssh/known_hosts
 ssh -R $DESTINATIONPORT:$LOCALIP:$LOCALPORT $DESTINATIONUSER@$DESTINATIONIP
